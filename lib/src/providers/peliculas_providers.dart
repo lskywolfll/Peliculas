fc5875ae031333ad9f696fe940f2cead190ed710
Rene <renealejandrosanchezmorales@gmail.com>
2020-10-24 14:55:35 -0300

    se termina la integracion de un provider para hacer las consultas http de la api de peliculas gratuita, se deja terminado la estructura de los datos a retornar, se instala dependencia http para realizar las consultas

---
+++ lib/src/providers/peliculas_providers.dart
@@ -0,0 +1,24 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:peliculas/src/models/pelicula_model.dart';
+
+class PeliculasProvider {
+  
+  String _apikey = "36f1042f84fedbe00c44c07f020e706b";
+  String _url = "api.themoviedb.org";
+  String _language = "es-ES";
+
+  Future<List<Pelicula>> getEnCines() async {
+    final url = Uri.https(_url, "3/movie/now_playing", {
+      "api_key": _apikey,
+      "language": _language
+    }); 
+
+    final resp = await http.get(url);
+    final decodedData = json.decode(resp.body);
+    final peliculas = new Peliculas.fromJsonList(decodedData["results"]);
+
+    return peliculas.items;
+
+  }
+}
