8b0c2a1c5fd8556f786ace9d5cb045877e7a0e40
Rene <renealejandrosanchezmorales@gmail.com>
2020-10-28 21:25:27 -0300

    se implemente la funcionalidad de streams informacion en una direccion hacia un final o desde distintitos puntos pero el mismo final

--- lib/src/providers/peliculas_providers.dart
+++ lib/src/providers/peliculas_providers.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:peliculas/src/models/pelicula_model.dart';
@@ -7,6 +8,22 @@ class PeliculasProvider {
   String _apikey = "36f1042f84fedbe00c44c07f020e706b";
   String _url = "api.themoviedb.org";
   String _language = "es-ES";
+
+  int _popularesPage = 0;
+
+  List<Pelicula> _populares = new List();
+
+  // single => solo se puede comunicar con uno
+  // broadcast => puede comunicarse con muchos
+  final _popularesStreamController = StreamController<List<Pelicula>>.broadcast();
+
+  Function(List<Pelicula>) get popularesSink => _popularesStreamController.sink.add;
+
+  Stream<List<Pelicula>> get popularesStream => _popularesStreamController.stream;
+
+  void disposeStreams(){
+    _popularesStreamController?.close();
+  }
 
   Future<List<Pelicula>> _procesarRespuesta(Uri url) async {
     final resp = await http.get(url);
@@ -26,11 +43,21 @@ class PeliculasProvider {
   }
 
   Future<List<Pelicula>> getPopulares() async {
+
+      _popularesPage++;
+
       final url = Uri.https(_url, "3/movie/popular", {
       "api_key": _apikey,
-      "language": _language
-    }); 
+      "language": _language,
+      "page": _popularesPage.toString()
+    });
 
-    return await _procesarRespuesta(url);
+    final resp = await _procesarRespuesta(url);
+
+    _populares.addAll(resp);
+
+    popularesSink(_populares);
+
+    return resp;
   }
 }
