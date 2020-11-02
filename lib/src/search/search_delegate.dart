fd1b608f397ff2184970cccb46773a056d33dd38
Rene <renealejandrosanchezmorales@gmail.com>
2020-11-02 15:58:50 -0300

    se comeinza la parte de integracion del buscador con la clase generica para usar en otros proyectos flutter, falta terminar el build results y sugerencias

---
+++ lib/src/search/search_delegate.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class DataSearch extends SearchDelegate {
+
+  // query es el valor actual que tiene el buscador del delegate
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    // Las acciones de nuestro app bar
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = "";
+        },
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    // Icono a la izquierda del appbar
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // Crea los resultados que vamos a mostrar
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // Son las sugerencias que aparecen cuando la persona escribe
+    return Container();
+  }
+}
