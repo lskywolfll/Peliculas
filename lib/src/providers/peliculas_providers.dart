77a2d41f0aa7c905725facb4161a300b0994d960
Rene <renealejandrosanchezmorales@gmail.com>
2020-11-02 20:35:26 -0300

    se termina el proyecto de peliculas

--- lib/src/providers/peliculas_providers.dart
+++ lib/src/providers/peliculas_providers.dart
@@ -97,4 +97,14 @@ class PeliculasProvider {
 
     return video.videos;
   }
+
+  Future<List<Pelicula>> buscarPelicula(String query) async {
+    final url = Uri.https(_url, "3/search/movie", {
+      "api_key": _apikey,
+      "language": _language,
+      "query": query
+    }); 
+
+   return await _procesarRespuesta(url);
+  }
 }
--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/src/models/pelicula_model.dart';
+import 'package:peliculas/src/providers/peliculas_providers.dart';
 
 class DataSearch extends SearchDelegate {
+
+  String seleccion = "";
+    final peliculasProvider = new PeliculasProvider();
+
+  final pelculas = [
+    "Spiderman",
+    "Aquaman",
+    "Batman",
+    "Shazam",
+    "Ironman",
+    "Capitan America",
+    "Ironman 2",
+
+  ];
+
+  final peliculasRecientes = [
+    "Spiderman",
+    "Capitan America"
+  ];
 
   // query es el valor actual que tiene el buscador del delegate
 
@@ -34,12 +55,77 @@ class DataSearch extends SearchDelegate {
   @override
   Widget buildResults(BuildContext context) {
     // Crea los resultados que vamos a mostrar
-    return Container();
+    return Center(
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        color: Colors.amberAccent,
+        child: Text(seleccion),
+      ),
+    );
   }
+
+  // @override
+  // Widget buildSuggestions(BuildContext context) {
+  //   // Son las sugerencias que aparecen cuando la persona escribe
+
+  //   final listaSugerida = (query.isEmpty) 
+  //     ? peliculasRecientes 
+  //     : pelculas.where((p) => p.toLowerCase().startsWith(query.toLowerCase())).toList();
+
+  //   return ListView.builder(
+  //     itemCount: listaSugerida.length,
+  //     itemBuilder: (context, i){
+  //       return ListTile(
+  //         leading: Icon(Icons.movie),
+  //         title: Text(listaSugerida[i]),
+  //         onTap: () {
+  //           seleccion = listaSugerida[i];
+  //         },
+  //       );
+  //     }
+  //   );
+  // }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     // Son las sugerencias que aparecen cuando la persona escribe
-    return Container();
+
+    if(query.isEmpty){
+      return Container();
+    }
+
+    return FutureBuilder(
+      future: peliculasProvider.buscarPelicula(query),
+      builder: (BuildContext context, AsyncSnapshot<List<Pelicula>> snapshot) {
+        
+        if(snapshot.hasData){
+
+          final peliculas = snapshot.data;
+          
+          return ListView(
+            children: peliculas.map((pelicula) {
+              return ListTile(
+                leading: FadeInImage(
+                  image: NetworkImage(pelicula.getPosterImg()),
+                  placeholder: AssetImage("asset/img/no-image.jpg"),
+                  width: 50.0,
+                  fit: BoxFit.contain,
+                ),
+                title: Text(pelicula.title),
+                subtitle: Text(pelicula.originalTitle),
+                onTap: () {
+                  close(context, null);
+                  pelicula.uniqueId = "";
+                  Navigator.pushNamed(context, "detalle", arguments: pelicula);
+                },
+              );
+            }).toList(),
+          );
+        }else{
+          return Center(child: CircularProgressIndicator());
+        }
+      },
+    );
   }
 }
